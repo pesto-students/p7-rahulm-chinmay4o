996f98dcc487d364e5c0d48011aba162584d910c
chinmay4o <surve4404@gmail.com>
2022-12-10 15:43:40 +0530

    completed iterators with fibonacci series

---
+++ Week-4/assignment-3/index.js
@@ -0,0 +1,46 @@
+// const fib = {
+//   [Symbol.iterator]: function () {
+//     let i = 0;
+//     let oldVal = 0;
+//     let newVal = 0;
+//     return {
+//       next() {
+//         oldVal = newVal > 0 ? newVal : 0;
+//         newVal = (oldVal + newVal) === 0 ? 1 : (oldVal + newVal) ;
+//         return {
+//           value: oldVal,
+//           done: i++ >= 10,
+//         };
+//       },
+//     };
+//   },
+// };
+// Just commenting above solution to update you with what i was thinking previously
+
+const fib = {
+  //this obj is now iterable
+  [Symbol.iterator]: function () {
+    let i = 0;
+    let val = 1;
+    let arr = [];
+    return {
+      next() {
+        if (i === 0) {
+          arr.push(0);
+        } else if (i === 1) {
+          arr.push(1);
+        } else {
+          arr.push(arr[i - 1] + arr[i - 2]);
+        }
+        return {
+          value: arr[i],
+          done: i++ >= 10,
+        };
+      },
+    };
+  },
+};
+
+for (let ele of fib) {
+  console.log(ele);
+}
