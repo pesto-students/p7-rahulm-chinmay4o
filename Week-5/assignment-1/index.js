852b429f9888188de1b16db6dddd0ecdfc3b0cf5
chinmay4o <surve4404@gmail.com>
2022-12-16 02:44:22 +0530

    added assignment 5.1 with generators

--- Week-5/assignment-1/index.js
+++ Week-5/assignment-1/index.js
@@ -1,10 +1,53 @@
-async function* fetchUsers() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  yield await response.json();
+import fetch from "node-fetch";
+
+//completing the tasks with Generators
+function doTask1() {
+  let result = fetch("https://jsonplaceholder.typicode.com/users");
+  result
+    .then((val) => {
+      return val.json();
+    })
+    .then((val) => {
+      console.log(val);
+      me.next();
+    });
 }
 
-const it = fetchUsers();
+function doTask2() {
+  setTimeout(() => {
+    console.log("completed task 2");
+    me.next();
+  }, 3000);
+}
 
-it.next().then(({ value, done }) => {
-  console.log(value);
-});
+function doTask3() {
+  setTimeout(() => {
+    return console.log("completed task 3");
+  }, 3000);
+}
+
+function* printResults() {
+  yield doTask1();
+
+  console.log("Done with task1");
+  yield doTask2();
+
+  console.log("Done with task2");
+  yield doTask3();
+}
+
+const me = printResults();
+me.next();
+
+// Completing the tasks with Async/Await
+// async function printAsyncResults() {
+//     await doTask1();
+
+//     console.log("Done with task1");
+//     await doTask2();
+
+//     console.log("Done with task2");
+//     await doTask3();
+// }
+
+// printAsyncResults();
