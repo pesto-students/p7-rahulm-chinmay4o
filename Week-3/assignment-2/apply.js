bfceb74d3b97cc300261b6da9c80fc3d7adb102d
chinmay4o <surve4404@gmail.com>
2023-01-10 08:56:25 +0530

    added explanation

--- Week-3/assignment-2/apply.js
+++ Week-3/assignment-2/apply.js
@@ -1,4 +1,11 @@
 
 const num = [2,4,5,7,10,13];
 
-console.log(Math.max.apply(null,num));
+console.log(Math.max.apply(null,num)); 
+/* 
+As math.max takes direct array and not a reference we can use apply
+method as The apply() method accepts arguments in an array
+Since JavaScript arrays do not have a max() method we can utilise apply for the same purpose
+if the first argument of the apply() method is not an object, it becomes the owner (object) of the invoked function. 
+In "non-strict" mode, it becomes the global object.
+*/
