f0d78f0cf75813456b23e79f970c964e7b0a6ee7
chinmay4o <surve4404@gmail.com>
2023-01-15 20:20:39 +0530

    BDD approach

--- Week-5/assignment-4/calculator.test.js
+++ Week-5/assignment-4/calculator.test.js
@@ -1,7 +1,13 @@
 const mathOperations = require("./index");
 
 test("add 2 numbers", () => {
-  expect(mathOperations.sum(3, 4)).toBe(7);
+  //given
+  const a = 3, b = 4;
+  //when
+  const result = mathOperations.sum(a, b);
+  //then
+  expect(result).toBe(7);
+  // expect(mathOperations.sum(3, 4)).toBe(7);
 });
 
 test("subtract 2 numbers", () => {
